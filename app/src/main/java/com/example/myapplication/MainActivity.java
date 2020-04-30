d626580a18538de3d00ce9245af864d64265be2a
Chalanika <chalanikasewwandi@gmail.com>
2020-04-30 19:11:50 +0530

    get inputs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,23 +6,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnAdd;
     EditText txt1;
+    EditText txt2;
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnAdd = findViewById(R.id.butAdd);
         txt1 = findViewById(R.id.input1);
+        txt2 = findViewById(R.id.input2);
+        result = findViewById(R.id.answer);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Added");
-                System.out.println(txt1.getText());
+                
             }
         });
     }
