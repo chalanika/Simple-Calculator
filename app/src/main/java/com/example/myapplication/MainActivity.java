4457381e2123b43e3504aab32d5a483b3969e3db
Chalanika <chalanikasewwandi@gmail.com>
2020-05-02 17:18:10 +0530

    added exception handling

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -31,8 +31,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 num1 = txt1.getText().toString();
                 num2 = txt2.getText().toString();
-                number1 = Double.parseDouble(num1);
-                number2 = Double.parseDouble(num2);
+                try {
+                    number1 = Double.parseDouble(num1);
+                    number2 = Double.parseDouble(num2);
+                }catch (Exception e){
+                    result.setText("Error");
+                }
             }
         });
     }
